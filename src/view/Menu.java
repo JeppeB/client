72cb83ac222d5ca71165bf964b5a779cbae4d5c1
JeppeB <jeppe.bendixen@gmail.com>
2014-12-07 13:49:45 +0100

    encryption added
    needs testing

---
+++ src/view/Menu.java
@@ -0,0 +1,12 @@
+package view;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class Menu extends Screen {
+
+	private ImageIcon Baggrundsbillede;
+	private JLabel Jlabel1; 
+	
+	
+}
